023454a47537848b05eb2ef5747c0fe874333cd8
aepick <47307041+aepick@users.noreply.github.com>
2019-06-28 12:17:57 -0400

    Add files via upload

---
+++ SimulationClient.py
@@ -0,0 +1,40 @@
+import socket
+import time
+import json
+import sys
+sensitiveData = {
+    'user':'MyUsername',
+    'password':'password1234'
+}
+connected = False
+counter = 0
+host = sys.argv[1]
+port = int(sys.argv[2])                   # The same port as used by the server
+while counter < 100:
+    #s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    #s.connect((host, port))
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.connect((host, port))
+        connected = True
+    except socket.error as error:
+        counter += 1
+        print('error occurred trying again: '+ str(counter))
+        connected = False
+        s.close()
+    if(connected == True):
+        try:
+            data=(json.dumps(sensitiveData))
+            #print(data)
+            s.sendall(data.encode('utf-8'))
+            data = s.recv(1024)
+            print('Received', repr(data))
+            connected = False
+            time.sleep(2)
+        except(KeyboardInterrupt):
+            data = 'Exit'
+            s.sendall(data.encode('utf-8'))
+            s.close()
+    
+    time.sleep(2)
+    
---
+++ SimulationServer.py
@@ -0,0 +1,27 @@
+import socket
+import sys
+import json
+host = sys.argv[1]        # Symbolic name meaning all available interfaces
+port = int(sys.argv[2])    # Arbitrary non-privileged port
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+
+print(host , port)
+while True:
+    s.listen(1)
+    conn, addr = s.accept()
+    print('Connected by', addr)
+    try:
+        data = conn.recv(1024)
+        if not data:
+            print('Not Data' + data.decode())
+            print(data) 
+            break
+        print ("Client Says: "+data.decode())
+        conn.sendall(b"Data Received")
+        if(data.decode() == 'Exit'):
+            break
+    except socket.error:
+        print ("Error Occured.")
+        break
+conn.close()
